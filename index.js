f84dc68f9a8034233710ad3a3106e4dce8b0771e
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-02-19 15:54:46 +0200

    style

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import 'maplibre-gl/dist/mapbox-gl.css';
 var map = new Map({
     container: 'map', // container id
     hash: true,
-    style: 'https://beta-karttakuva.maanmittauslaitos.fi/kipa/kiinteistojaotus_mobile_beta1/style-3857.json',
-    center: [24.937048, 60.198872], // starting position [lng, lat]
-    zoom: 15.29, // starting zoom
+    style: 'https://beta-karttakuva.maanmittauslaitos.fi/kipa/WGS84_Pseudo-Mercator/kipa_kiinteistojaotus_simple_v1.json'
 });
     
